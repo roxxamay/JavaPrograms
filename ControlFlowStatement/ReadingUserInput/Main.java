293b50647e6f031dc6e41fa40b142899a158ac86
roxxamay <itsmeadityakumararya@gmail.com>
2023-07-16 14:16:27 +0530

    initial build

--- ControlFlowStatement/ReadingUserInput/Main.java
+++ ControlFlowStatement/ReadingUserInput/Main.java
@@ -2,14 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in)
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter of the BIRTH year : ");
-        int yearOfBirth = scanner.nextInt(0);
-        scanner.nextLine();      //handle the next line character [enter key error]
 
-        System.out.println("Enter Your Name : ");
-        String name = scanner.nextLine();
-        int age = 
+        boolean hasNextInt = scanner.hasNextInt();
+
+        if(hasNextInt){
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine();      //handle the next line character [enter key error]
+
+            System.out.println("Enter Your Name : ");
+            String name = scanner.nextLine();
+            int age = 2023 - yearOfBirth ;
+
+            if(age > 0 && age <=100){
+                System.out.println("Your name is " + name + " and your age is " + age);
+
+            }
+            else{
+                System.out.println("Invalid year");
+            }
+
+        }
+        scanner.close();
     }
+
 }
